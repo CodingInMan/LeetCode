7c18f2fd02efa097b7cdb5b880d03759d41079b1
张桂源 <zhanggy@tuya.com>
2020-04-23 19:44:37 +0800

    创建一个新工程

---
+++ LeetCode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LeetCode
+//
+//  Created by 帕克 on 2020/4/23.
+//  Copyright © 2020 帕克. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
